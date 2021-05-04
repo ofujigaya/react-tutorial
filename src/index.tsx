92b1fa16e3ca333cef271c1cd5c258a1e8da1e02
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2021-05-05 06:15:55 +0900

    Taking Turns

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ type BoardProps = {
 }
 type BoardState = {
   squares: string[]
+  xIsNext: boolean
 }
 const squares = Array<string>(9).fill('')
 class Board extends React.Component<BoardProps, BoardState> {
@@ -26,15 +27,17 @@ class Board extends React.Component<BoardProps, BoardState> {
     super(props)
     this.state = {
       squares: squares,
+      xIsNext: true,
     }
   }
 
   handleClick(i: number) {
     console.log(i) // open and check the DevTools
     const squares: Array<string> = this.state.squares.slice()
-    squares[i] = 'X'
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
+      xIsNext: !this.state.xIsNext,
     })
     console.log(this.state) // open and check the DevTools
   }
@@ -44,7 +47,7 @@ class Board extends React.Component<BoardProps, BoardState> {
   }
 
   render() {
-    const status = 'Next player: X'
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
 
     return (
       <div>

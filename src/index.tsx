aea209b77b1686364a89931b4af022dbe7c58fc1
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2021-05-05 04:33:13 +0900

    Get started React Tutorial
    - Ref: https://reactjs.org/tutorial/tutorial.html
    - Inspecting the Starter Code
    - Passing Data Through Props

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class Square extends React.Component {
+type SquareProps = {
+  value: number
+}
+class Square extends React.Component<SquareProps> {
   render() {
-    return <button className="square">{/* TODO */}</button>
+    return <button className="square">{this.props.value}</button>
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />
+  renderSquare(i: number) {
+    return <Square value={i} />
   }
 
   render() {

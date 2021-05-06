d98b895e719d2667c601b7f5db8eb62b0cfd297e
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2021-05-06 13:05:02 +0900

    Refactor Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,27 +2,18 @@ import Square from './Square'
 import { BoardProps } from '../types/interface'
 
 const Board: React.FC<BoardProps> = ({ squares, onClick }) => {
-  const renderSquare = (i: number) => {
-    return <Square value={squares[i]} onClick={() => onClick(i)} />
-  }
-
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {[...Array(3)].map((_, i) => {
+        return (
+          <div className="board-row" key={i}>
+            {[...Array(3)].map((_, j) => {
+              const index = 3 * i + j
+              return <Square value={squares[index]} onClick={() => onClick(index)} key={j}></Square>
+            })}
+          </div>
+        )
+      })}
     </div>
   )
 }

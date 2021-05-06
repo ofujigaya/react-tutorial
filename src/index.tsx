46a7ceb505ca8d6b49c0ab3a8e9b5d3149032e95
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2021-05-06 19:13:57 +0900

    Devide components in `index.tsx` into each component

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,5 @@
 import ReactDOM from 'react-dom'
+import Game from './components/Game'
 import './index.css'
-
-const Square = () => {
-  return <button className="square">{/* TODO */}</button>
-}
-
-const Board = () => {
-  const status = 'Next player: X'
-
-  const renderSquare = (i) => {
-    return <Square />
-  }
-
-  return (
-    <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-    </div>
-  )
-}
-
-const Game = () => {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  )
-}
-// ========================================
 
 ReactDOM.render(<Game />, document.getElementById('root'))

764d4b91e2e7c0801e5dad460f1dcaa72e9721f4
Yousuke Fujigaya <fujigaya.yousuke@gmail.com>
2021-05-08 00:20:36 +0900

    Completed up to the section of Adding Time Travel

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,62 +1,23 @@
-import { useState } from 'react'
 import Square from './Square'
 import { SquareType } from '../types'
 
-export default function Board() {
-  const [_squares, setSquares] = useState(Array<SquareType>(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
-
-  const handleClick = (i: number) => {
-    const squares = _squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = xIsNext ? 'X' : 'O'
-    setSquares(squares)
-    setXIsNext(!xIsNext)
-  }
-
-  const winner = calculateWinner(_squares)
-  let status
-  if (winner) {
-    status = 'Winner:' + winner
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
-  }
-
+interface BoardProps {
+  squares: SquareType[]
+  onClick: (i: number) => void
+}
+export default function Board(props: BoardProps) {
   return (
     <div>
-      <div className="status">{status}</div>
       {[...Array(3)].map((_, i) => {
         return (
           <div className="board-row" key={i}>
             {[...Array(3)].map((_, j) => {
               const index: number = 3 * i + j
-              return <Square value={_squares[index]} onClick={() => handleClick(index)} key={j} />
+              return <Square value={props.squares[index]} onClick={() => props.onClick(index)} key={j} />
             })}
           </div>
         )
       })}
     </div>
   )
-}
-
-function calculateWinner(squares: SquareType[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ]
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,59 @@
+import { useState } from 'react'
 import Board from './Board'
+import { calculateWinner } from '../logic'
+import { SquareType } from '../types'
+
+type HistoryType = { squares: SquareType[] }[]
 
 export default function Game() {
+  const [histories, setHistories] = useState<HistoryType>([{ squares: Array(9).fill(null) }])
+  const [stepNumber, setStepNumber] = useState(0)
+  const [xIsNext, setXIsNext] = useState(true)
+
+  const handleClick = (i: number) => {
+    const history = histories.slice(0, stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = xIsNext ? 'X' : 'O'
+    setHistories(history.concat([{ squares: squares }]))
+    setStepNumber(history.length)
+    setXIsNext(!xIsNext)
+  }
+
+  const jumpTo = (step: number) => {
+    setStepNumber(step)
+    setXIsNext(step % 2 === 0)
+  }
+
+  const current = histories[stepNumber]
+  const winner = calculateWinner(current.squares)
+  let status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+  }
+
+  const moves = histories.map((_step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start'
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
